1daf9a3f71a2fc59054fd6bac9c082687fcd7b64
dhViet2004 <22671321.viet@student.iuh.edu.vn>
2025-01-17 12:21:45 +0700

    Done test CRUD TuyenTau

--- Project_BanVeTauTaiGasLacHong/src/main/java/dao/TuyenTauDAO.java
+++ Project_BanVeTauTaiGasLacHong/src/main/java/dao/TuyenTauDAO.java
@@ -11,12 +11,17 @@ import java.util.List;
 @AllArgsConstructor
 public class TuyenTauDAO {
     private EntityManager em;
-    public List<TuyenTau> getListTuyenTau(String gaDi, String gaDen) {
+    public List<TuyenTau> getListTuyenTauByGaDiGaDen(String gaDi, String gaDen) {
         return em.createNamedQuery("TuyenTau.findByGaDiGaDen", TuyenTau.class)
                 .setParameter("gaDi", gaDi)
                 .setParameter("gaDen",gaDen)
                 .getResultList();
     }
+
+    public TuyenTau getTuyenTauById(String id) {
+        return em.find(TuyenTau.class, id);
+    }
+
     public  boolean save(TuyenTau tuyenTau){
         EntityTransaction tr = em.getTransaction();
         try {
@@ -44,5 +49,17 @@ public class TuyenTauDAO {
         }
         return false;
     }
-
+    public  boolean update(TuyenTau tuyenTau){
+        EntityTransaction tr = em.getTransaction();
+            try {
+                tr.begin();
+                em.merge(tuyenTau);
+                tr.commit();
+                return true;
+            }catch (Exception e){
+                e.printStackTrace();
+                tr.rollback();
+            }
+            return false;
+    }
 }
--- Project_BanVeTauTaiGasLacHong/src/main/java/testCRUD/Test_HV.java
+++ Project_BanVeTauTaiGasLacHong/src/main/java/testCRUD/Test_HV.java
@@ -11,22 +11,37 @@ public class Test_HV {
         EntityManager em = Persistence.createEntityManagerFactory("mariadb").createEntityManager();
 
         Faker faker = new Faker();
-        TuyenTau tuyenTau = new TuyenTau();
-        tuyenTau.setMaTuyen("TT1111");
-        tuyenTau.setTenTuyen("Tuyến " + faker.address().cityName() + " - " + faker.address().cityName()); // Tên tuyến với thành phố ngẫu nhiên
-        tuyenTau.setGaDi(faker.address().streetName()); // Ga đi ngẫu nhiên
-        tuyenTau.setGaDen(faker.address().streetName()); // Ga đến ngẫu nhiên
-        tuyenTau.setDiaDiemDi(faker.address().cityName() + ", " + faker.address().country()); // Địa điểm đi (thành phố, quốc gia)
-        tuyenTau.setDiaDiemDen(faker.address().cityName() + ", " + faker.address().country()); // Địa điểm đến (thành phố, quốc gia)
+//        <--------CRUD Tuyến Tàu ---------->
+//        TuyenTauDAO tuyenTauDAO = new TuyenTauDAO(em);
+//            <---------SAVE-------->
+//        TuyenTau tuyenTau = new TuyenTau();
 
-        TuyenTauDAO tuyenTauDAO = new TuyenTauDAO(em);
-        if (tuyenTauDAO.save(tuyenTau)){
-            System.out.println("Lưu thành công");
-        }else {
-            System.out.println("Lưu thất bại");
-        }
+//        tuyenTau.setMaTuyen("TT1111");
+//        tuyenTau.setTenTuyen("Tuyến " + faker.address().cityName() + " - " + faker.address().cityName()); // Tên tuyến với thành phố ngẫu nhiên
+//        tuyenTau.setGaDi(faker.address().streetName()); // Ga đi ngẫu nhiên
+//        tuyenTau.setGaDen(faker.address().streetName()); // Ga đến ngẫu nhiên
+//        tuyenTau.setDiaDiemDi(faker.address().cityName() + ", " + faker.address().country()); // Địa điểm đi (thành phố, quốc gia)
+//        tuyenTau.setDiaDiemDen(faker.address().cityName() + ", " + faker.address().country()); // Địa điểm đến (thành phố, quốc gia)
+//
+//        if (tuyenTauDAO.save(tuyenTau)){
+//            System.out.println("Lưu thành công");
+//        }else {
+//            System.out.println("Lưu thất bại");
+//        }
+//        TuyenTau ttToUpdate = tuyenTauDAO.getTuyenTauById("TT1111");
+//        System.out.println(ttToUpdate);
+//      <----------UPDATE----->
+//        if(ttToUpdate != null) {
+//            ttToUpdate.setGaDi("HCM");
+//            ttToUpdate.setGaDen("Ha Noi");
+//            boolean resultUpdate = tuyenTauDAO.update(ttToUpdate);
+//            System.out.println(resultUpdate ? "Cập nhật thành công" : "Cập nhật thất bại");
+//        }
 
 
+//        <-------DELETE--------->
+//        boolean resultDelete = tuyenTauDAO.delete("TT1111");
+//        System.out.println(resultDelete ? "Xóa thành công" : "Xóa thất bại");
 
 
         em.close();
